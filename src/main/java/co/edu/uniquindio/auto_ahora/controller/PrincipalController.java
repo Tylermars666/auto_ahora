4d9f0818af83d7271e79cea6f242b2c90accabc0
Omar <omar.piedrahitat@gmail.com>
2023-08-13 18:45:42 -0500

    Actualizo conexión entre ventana principal y ventana registro

--- src/main/java/co/edu/uniquindio/auto_ahora/controller/PrincipalController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/PrincipalController.java
@@ -2,7 +2,12 @@ package co.edu.uniquindio.auto_ahora.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PrincipalController {
 
@@ -37,7 +42,17 @@ public class PrincipalController {
     }
 
     @FXML
-    void abrirMenuRegistro(ActionEvent event) {
+    void abrirMenuRegistro(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/auto_ahora/view/registro-view.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Registros");
+        stage.setScene(scene);
+
+        RegistroController controller = loader.getController();
+        stage.show();
 
     }
 
