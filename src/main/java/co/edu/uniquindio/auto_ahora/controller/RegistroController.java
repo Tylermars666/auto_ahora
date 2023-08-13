064d8f5049beeb6bda32c5f26100b479feca2492
Omar <omar.piedrahitat@gmail.com>
2023-08-13 17:48:33 -0500

    Creo conexión desde la ventana principal a las ventanas de registro moto, auto y camion

--- src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroController.java
@@ -2,7 +2,12 @@ package co.edu.uniquindio.auto_ahora.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class RegistroController {
 
@@ -16,18 +21,42 @@ public class RegistroController {
     private Button btnMoto;
 
     @FXML
-    void abrirRegistroAutomovil(ActionEvent event) {
+    void abrirRegistroAutomovil(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/auto_ahora/view/registro-auto-view.fxml"));
 
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Registro Auto");
+        stage.setScene(scene);
+
+        RegistroAutoController controller = loader.getController();
+        stage.show();
     }
 
     @FXML
-    void abrirRegistroCamion(ActionEvent event) {
+    void abrirRegistroCamion(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/auto_ahora/view/registro-camion-view.fxml"));
 
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Registro Camion");
+        stage.setScene(scene);
+
+        RegistroCamionController controller = loader.getController();
+        stage.show();
     }
 
     @FXML
-    void abrirRegistroMoto(ActionEvent event) {
+    void abrirRegistroMoto(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/auto_ahora/view/registro-moto-view.fxml"));
 
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Registro Moto");
+        stage.setScene(scene);
+
+        RegistroMotoController controller = loader.getController();
+        stage.show();
     }
 
 }
