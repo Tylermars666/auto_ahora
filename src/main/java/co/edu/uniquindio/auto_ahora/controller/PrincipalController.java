49afc4170220be39064e9d9fc95eac048ff364c9
CarlosMancera <carlosh.mancerar@uqvirtual.edu.co>
2023-08-13 18:35:52 -0500

    Se quita el boton salir de la pantalla principal

--- src/main/java/co/edu/uniquindio/auto_ahora/controller/PrincipalController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/PrincipalController.java
@@ -2,12 +2,7 @@ package co.edu.uniquindio.auto_ahora.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class PrincipalController {
 
@@ -27,9 +22,6 @@ public class PrincipalController {
     private Button btn_menu_ventas;
 
     @FXML
-    private Button btn_salir;
-
-    @FXML
     void abrirMenuActualizar(ActionEvent event) {
 
     }
@@ -45,27 +37,12 @@ public class PrincipalController {
     }
 
     @FXML
-    void abrirMenuRegistro(ActionEvent event) throws IOException {
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/auto_ahora/view/registro-view.fxml"));
-
-        Scene scene = new Scene(loader.load());
-        Stage stage = new Stage();
-        stage.setTitle("Registros");
-        stage.setScene(scene);
-
-        RegistroController controller = loader.getController();
-        stage.show();
+    void abrirMenuRegistro(ActionEvent event) {
 
     }
 
     @FXML
     void abrirMenuVentas(ActionEvent event) {
-
-    }
-
-    @FXML
-    void cerrarAplicacion(ActionEvent event) {
 
     }
 
