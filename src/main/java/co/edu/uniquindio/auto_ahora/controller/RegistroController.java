0516ba976edc9f03b5b750951f628b224e560dfc
CarlosMancera <carlosh.mancerar@uqvirtual.edu.co>
2023-08-13 16:15:43 -0500

    Se realiza Modifiacion al Registro Controller por SceneBuilder

--- src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroController.java
@@ -1,6 +1,17 @@
 package co.edu.uniquindio.auto_ahora.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class RegistroController {
 
+    @FXML
+    private Button btnAutomovil;
+
+    @FXML
+    private Button btnCamion;
+
+    @FXML
+    private Button btnMoto;
 
 }
