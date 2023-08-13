9985da764b3b0ab6ca4435db454936594605e4e3
CarlosMancera <carlosh.mancerar@uqvirtual.edu.co>
2023-08-13 17:25:51 -0500

    Se crean Registro Controlador --> Moto, Camión y Automovil

--- src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroAutoController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroAutoController.java
@@ -1,4 +1,33 @@
 package co.edu.uniquindio.auto_ahora.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class RegistroAutoController {
+
+    @FXML
+    private TextField txtIsGasolina;
+
+    @FXML
+    private TextField txtMarca;
+
+    @FXML
+    private TextField txtModelo;
+
+    @FXML
+    private TextField txtNumPuertas;
+
+    @FXML
+    private TextField txtNumRuedas;
+
+    @FXML
+    private TextField txtPlaca;
+
+    @FXML
+    private TextField txtPrecio;
+
+    @FXML
+    private TextField txtReferencia;
+
 }
+    
--- src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroCamionController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/RegistroCamionController.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.auto_ahora.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class RegistroCamionController {
+
+    @FXML
+    private TextField txtCap_Carga;
+
 }
