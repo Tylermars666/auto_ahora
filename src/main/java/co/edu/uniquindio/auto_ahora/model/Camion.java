4373c70280291c9cbc3ba04a2833fb1e96a34f47
Omar <omar.piedrahitat@gmail.com>
2023-08-14 12:36:28 -0500

    Doy atributos y métodos a la clase del modelo Camion

--- src/main/java/co/edu/uniquindio/auto_ahora/model/Camion.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/model/Camion.java
@@ -1,7 +1,36 @@
 package co.edu.uniquindio.auto_ahora.model;
 
-public class Camion {
+public class Camion extends Vehiculo {
 
+    int cap_carga;
 
+    public Camion (){}
 
+    public Camion (String placa, String marca, String referencia, String modelo, int num_ruedas,double precio, int cap_carga){
+
+        super(placa, marca, referencia, modelo, num_ruedas, precio);
+        this.cap_carga = cap_carga;
+    }
+
+    public int getCap_carga() {
+        return cap_carga;
+    }
+
+    public void setCap_carga(int cap_carga) {
+        this.cap_carga = cap_carga;
+    }
+
+    @Override
+    public String toString(){          //Método toString para imprimir en consola los valores del objeto creado.
+
+        return "Moto \n" +
+                "Precio: " + this.getPrecio() + "\n" +
+                "Placa: " + this.getPlaca() + "\n" +
+                "Marca: " + this.getMarca() + "\n" +
+                "Referencia: " + this.getReferencia() + "\n" +
+                "Modelo: " + this.getModelo() + "\n" +
+                "Numero de Ruedas: " + this.getNum_ruedas() + "\n" +
+                "Capacidad de Carga: " + cap_carga;
+
+    }
 }
