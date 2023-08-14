479d29387979b141bb1c7b4b2dce72f5639faa0d
Omar <omar.piedrahitat@gmail.com>
2023-08-13 20:59:41 -0500

    Creo atributos y métodos getter y setter para la clase DataBase que funcionará como base de datos

--- src/main/java/co/edu/uniquindio/auto_ahora/persistence/DataBase.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/persistence/DataBase.java
@@ -1,6 +1,30 @@
 package co.edu.uniquindio.auto_ahora.persistence;
 
+import co.edu.uniquindio.auto_ahora.model.Vehiculo;
+import co.edu.uniquindio.auto_ahora.model.Venta;
+
+import java.util.ArrayList;
+
 public class DataBase {
 
+    private ArrayList<Vehiculo> listaVehiculos;
+    private ArrayList<Venta> listaVentas;
 
+    public DataBase(){}
+
+    public ArrayList<Vehiculo> getListaVehiculos(){
+        return this.listaVehiculos;
+    }
+
+    public ArrayList<Venta> getListaVentas(){
+        return this.listaVentas;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo){
+        listaVehiculos.add(vehiculo);
+    }
+
+    public void setVenta(Venta venta){
+        listaVentas.add(venta);
+    }
 }
