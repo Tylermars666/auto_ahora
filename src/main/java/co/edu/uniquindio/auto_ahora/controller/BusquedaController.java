6d6e80da8906ee5d9f0147b7e02137a57cd67bc8
Omar <omar.piedrahitat@gmail.com>
2023-08-14 14:19:23 -0500

    Agrego elementos a busqueda-view en escene builder y copio skelleton en BusquedaController

--- src/main/java/co/edu/uniquindio/auto_ahora/controller/BusquedaController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/BusquedaController.java
@@ -1,4 +1,48 @@
 package co.edu.uniquindio.auto_ahora.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class BusquedaController {
+
+    @FXML
+    private Button btn_filtro;
+
+    @FXML
+    private ComboBox<?> cmb_filtro_busqueda;
+
+    @FXML
+    private TableColumn<?, ?> col_marca;
+
+    @FXML
+    private TableColumn<?, ?> col_modelo;
+
+    @FXML
+    private TableColumn<?, ?> col_numero_ruedas;
+
+    @FXML
+    private TableColumn<?, ?> col_placa;
+
+    @FXML
+    private TableColumn<?, ?> col_precio;
+
+    @FXML
+    private TableColumn<?, ?> col_referencia;
+
+    @FXML
+    private TableView<?> tbt_lista_vehiculos;
+
+    @FXML
+    private TextField txt_valor_filtro;
+
+    @FXML
+    void aplicarFiltroBusqueda(ActionEvent event) {
+
+    }
+
 }
