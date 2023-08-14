d621791ccede614ec75e04815ac6b8b82b6b10f6
Omar <omar.piedrahitat@gmail.com>
2023-08-14 17:07:48 -0500

    Añado métodos para filtrar por texto escogiendo el atributo en la clase BusquedaController

--- src/main/java/co/edu/uniquindio/auto_ahora/controller/BusquedaController.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/controller/BusquedaController.java
@@ -1,29 +1,30 @@
 package co.edu.uniquindio.auto_ahora.controller;
 
+import javafx.event.ActionEvent;
 import co.edu.uniquindio.auto_ahora.model.Concesionario;
 import co.edu.uniquindio.auto_ahora.model.Vehiculo;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BusquedaController implements Initializable {
 
-    @FXML
-    private Button btn_filtro;
+    String filtro_seleccionado;
+
+    String valor_filtro;
 
     @FXML
-    private ComboBox<?> cmb_filtro_busqueda;
+    private ComboBox<String> cmb_filtro_busqueda;
 
     @FXML
     private TableColumn<?, ?> col_marca;
@@ -49,18 +50,132 @@ public class BusquedaController implements Initializable {
     @FXML
     private TextField txt_valor_filtro;
 
-    private ObservableList<Vehiculo> vehiculos;
+    private ObservableList<Vehiculo> vehiculos;                     //Observable List para tabla de vehiculos
+    private ObservableList<Vehiculo> filtro_vehiculos;              //Observable List para tabla aplicando filtro
+    private ObservableList<String> opciones_filtro;                 //Observable List para el Combobox
 
     @FXML
-    void aplicarFiltroBusqueda(ActionEvent event) {
+    void seleccionarFiltro(ActionEvent event) {
+
+        //placa, marca, modelo, referencia, precio
+        filtro_seleccionado = cmb_filtro_busqueda.getSelectionModel().getSelectedItem();
 
 
+
+    }
+
+    @FXML
+    void filtrarTexto(KeyEvent event) {
+
+        switch (filtro_seleccionado){
+
+            case "placa":
+               filtrarPorPlaca();
+                break;
+
+            case "marca":
+               filtrarPorMarca();
+                break;
+
+            case "referencia":
+                filtrarPorReferencia();
+                break;
+
+            case "modelo":
+                filtrarPorModelo();
+                break;
+
+            case "precio":
+                filtrarPorPrecio();
+        }
+
+
+
+    }
+
+    public void filtrarPorPlaca(){
+
+        valor_filtro = this.txt_valor_filtro.getText();
+        if(valor_filtro.isEmpty()){
+            this.tbt_lista_vehiculos.setItems(vehiculos);
+        }else{
+            this.filtro_vehiculos.clear();
+            for(Vehiculo vehiculo : this.vehiculos){
+                if(vehiculo.getMarca().contains(valor_filtro.toLowerCase())){
+                    this.filtro_vehiculos.add(vehiculo);
+                }
+            }
+            this.tbt_lista_vehiculos.setItems(filtro_vehiculos);
+        }
+    }
+
+    public void filtrarPorMarca(){
+        valor_filtro = this.txt_valor_filtro.getText();
+        if(valor_filtro.isEmpty()){
+            this.tbt_lista_vehiculos.setItems(vehiculos);
+        }else{
+            this.filtro_vehiculos.clear();
+            for(Vehiculo vehiculo : this.vehiculos){
+                if(vehiculo.getMarca().contains(valor_filtro.toLowerCase())){
+                    this.filtro_vehiculos.add(vehiculo);
+                }
+            }
+            this.tbt_lista_vehiculos.setItems(filtro_vehiculos);
+        }
+    }
+    public void filtrarPorReferencia(){
+        valor_filtro = this.txt_valor_filtro.getText();
+        if(valor_filtro.isEmpty()){
+            this.tbt_lista_vehiculos.setItems(vehiculos);
+        }else{
+            this.filtro_vehiculos.clear();
+            for(Vehiculo vehiculo : this.vehiculos){
+                if(vehiculo.getReferencia().contains(valor_filtro.toLowerCase())){
+                    this.filtro_vehiculos.add(vehiculo);
+                }
+            }
+            this.tbt_lista_vehiculos.setItems(filtro_vehiculos);
+        }
+    }
+    public void filtrarPorModelo(){
+        valor_filtro = this.txt_valor_filtro.getText();
+        if(valor_filtro.isEmpty()){
+            this.tbt_lista_vehiculos.setItems(vehiculos);
+        }else{
+            this.filtro_vehiculos.clear();
+            for(Vehiculo vehiculo : this.vehiculos){
+                if(vehiculo.getModelo().contains(valor_filtro.toLowerCase())){
+                    this.filtro_vehiculos.add(vehiculo);
+                }
+            }
+            this.tbt_lista_vehiculos.setItems(filtro_vehiculos);
+        }
+
+    }
+    public void filtrarPorPrecio(){
+        valor_filtro = this.txt_valor_filtro.getText();
+
+        if(valor_filtro.isEmpty()){
+            this.tbt_lista_vehiculos.setItems(vehiculos);
+        }else{
+            this.filtro_vehiculos.clear();
+            for(Vehiculo vehiculo : this.vehiculos){
+                if(String.valueOf(vehiculo.getPrecio()).contains(valor_filtro.toLowerCase())){
+                    this.filtro_vehiculos.add(vehiculo);
+                }
+            }
+            this.tbt_lista_vehiculos.setItems(filtro_vehiculos);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        vehiculos = FXCollections.observableArrayList();                                         //Lista para cargar en la tabla
+        opciones_filtro = FXCollections.observableArrayList("placa","marca","referencia","modelo","precio"); //Lineas para agregar
+        cmb_filtro_busqueda.setItems(opciones_filtro);                                                     //opciones al combobox
+
+        vehiculos = FXCollections.observableArrayList();
+        filtro_vehiculos = FXCollections.observableArrayList();                                 //Lista para cargar en la tabla
 
         this.col_placa.setCellValueFactory(new PropertyValueFactory<>("placa"));               //Estas lineas son para indicarle
         this.col_marca.setCellValueFactory(new PropertyValueFactory<>("marca"));               //a la tabla de la ventana de busqueda
@@ -69,10 +184,11 @@ public class BusquedaController implements Initializable {
         this.col_precio.setCellValueFactory(new PropertyValueFactory<>("precio"));
         this.col_numero_ruedas.setCellValueFactory(new PropertyValueFactory<>("num_ruedas"));
 
-        for(Vehiculo vehiculo : Concesionario.getInstancia().getDataBase().getListaVehiculos()){    //Este ciclo recorre los objetos
-            this.vehiculos.add(vehiculo);                                                           //y los va agregando a la tabla
-            this.tbt_lista_vehiculos.setItems(vehiculos);                                          //en este caso, todos los vehiculos
-        }
+        this.vehiculos.addAll(Concesionario.getInstancia().getDataBase().getListaVehiculos());  //Este es un método que recorre los objetos
+        this.tbt_lista_vehiculos.setItems(vehiculos);                                           //y los va agregando a la tabla
+                                                                                                //en este caso, todos los vehiculos
+
+
 
 
     }
