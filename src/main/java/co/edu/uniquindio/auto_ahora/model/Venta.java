c3c66608acc2d5bb6951d045f700944c634def97
Omar <omar.piedrahitat@gmail.com>
2023-08-15 16:47:19 -0500

    Agrego atributos de comprador y fecha a la clase Venta

--- src/main/java/co/edu/uniquindio/auto_ahora/model/Venta.java
+++ src/main/java/co/edu/uniquindio/auto_ahora/model/Venta.java
@@ -1,8 +1,12 @@
 package co.edu.uniquindio.auto_ahora.model;
 
+import java.time.LocalDate;
+
 public class Venta {
 
     private String codigo;
+    private String comprador;
     private Vehiculo vehiculo;
+    private LocalDate fecha;
 
 }
